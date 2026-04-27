31ea77ecd10c353eaeca5f91159b9907e6323318
awais <your_email@example.com>
2026-04-27 12:06:37 +0200

    lab report 1st week

--- numerical_approach/func.py
+++ numerical_approach/func.py
@@ -3,11 +3,5 @@
 #--------------------------------------------------
 import numpy as np
 
-
-def gaussian(x, A, mu, sigma):
-    """
-    Gaussian function: 
-    g(x)=A * e^{-(x-mu)^2 / (2*sigma^2)} 
-
-    """
-    return A * np.exp(-(x-mu)**2 / (2*sigma**2))
+def laser():
+    return
